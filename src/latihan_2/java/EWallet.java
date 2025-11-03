820c7f5709ae45b93003520bee8f7fc567e9c0f0
Belicia <2482020@unai.edu>
2025-11-03 10:37:18 +0700

    adding files

---
+++ src/latihan_2/java/EWallet.java
@@ -0,0 +1,8 @@
+package latihan2;
+
+// Implementasikan class EWallet implements Pembayaran
+public class EWallet implements Pembayaran {
+    // private String nomorTelepon;
+
+    // Constructor dan implement methods
+}
